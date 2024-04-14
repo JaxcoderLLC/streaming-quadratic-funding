b43db4c4a8b3603580c741c389cdeabc2c0b3404
Jaxcoder <codenamejason@outlook.com>
2024-04-13 21:47:39 -0400

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
       <Container fluid>
         <Row className="justify-content-between align-items-center w-100">
           <Col xs={5}>
-            <h1 className="fs-2 ps-2">Josh Leavitt for Nevada</h1>
+            <h1 className="fs-2 ps-2">Donate & Debate</h1>
           </Col>
           <Col xs="2" className="d-flex justify-content-end align-items-center">
             <ConnectWallet />
